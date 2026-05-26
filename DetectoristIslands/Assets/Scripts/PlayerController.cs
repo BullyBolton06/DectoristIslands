ed4098c1272f64b5e539fe557a6bf20ddc110321
Chris Bolton <cabolton51@gmail.com>
2026-05-26 16:00:36 +0100

    Player is now moving with WASD and rotates towards moving direction

--- DetectoristIslands/Assets/Scripts/PlayerController.cs
+++ DetectoristIslands/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInputs playerInputs;
+    private CharacterController movementController;
+    [SerializeField] private float playerSpeedMultiplier;
+    [SerializeField] private float playerRotateSpeed;
+    private Vector3 lastMovementDirection;
+    
 
     public void Start()
     {
-        playerInputs = new PlayerInputs();
+        movementController = GetComponent<CharacterController>();
+        lastMovementDirection = movementController.transform.forward; // set it to the current transform
+    }
+
+    private void Update()
+    {
+        var movement = InputManager.instance.playerInputs.Detecting.MoveCharacter.ReadValue<Vector2>();
+        if (movement != Vector2.zero)
+        {
+            
+            var movementVector = new Vector3(movement.x, 0, movement.y);
+            lastMovementDirection = movementVector;
+            movementController.Move(movementVector * (Time.deltaTime * playerSpeedMultiplier));
+        }
         
+        TurnToFaceDirection();
+       
+    }
+
+    private void TurnToFaceDirection()
+    {
+        //convert lastMovementDirection to an angle
+        var targetRotation = Quaternion.LookRotation(lastMovementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * playerRotateSpeed);
     }
 }
