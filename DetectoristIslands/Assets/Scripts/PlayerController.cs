8ae11001498546d045283f95009cdbaec77f38f7
Chris Bolton <cabolton51@gmail.com>
2026-05-27 13:58:24 +0100

    made it so the controls move the player correctly according to the camera angle

--- DetectoristIslands/Assets/Scripts/PlayerController.cs
+++ DetectoristIslands/Assets/Scripts/PlayerController.cs
@@ -15,13 +15,20 @@ public class PlayerController : MonoBehaviour
         lastMovementDirection = movementController.transform.forward; // set it to the current transform
     }
 
+    [SerializeField] private float cameraAngleModifier = 22.5f;
+
     private void Update()
     {
         var movement = InputManager.instance.playerInputs.Detecting.MoveCharacter.ReadValue<Vector2>();
         if (movement != Vector2.zero)
         {
-            
+            //we need to take the camera angle into account, where 1,0,0 should actually equal 0.77,0,0.77
+            //using quaternions, we can rotate our vector
             var movementVector = new Vector3(movement.x, 0, movement.y);
+            
+            var rotation = Quaternion.AngleAxis(cameraAngleModifier, Vector3.up);
+            
+            movementVector = rotation * movementVector;
             lastMovementDirection = movementVector;
             movementController.Move(movementVector * (Time.deltaTime * playerSpeedMultiplier));
         }
